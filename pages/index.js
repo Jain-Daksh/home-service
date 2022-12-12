ecab906558503c790220cdeb076b928ad703f0e2
Jain-Daksh <daksh@gmail.com>
2022-12-12 16:43:52 +0530

    navbar added

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Heading from '../pages/component/Heading'
+import Nav from './component/Navbar'
+
 export default function Home() {
   return (
     <>
-    <Heading />
+    <Nav />
     </>
   )
 }
